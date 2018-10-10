583cbaddb54827202e77f572b72b8cdfefcbb7aa
Youssef Gamil <yoga1290@gmail.com>
2018-10-10 14:36:06 +0000

    Update docker-run.sh

--- server/docker-run.sh
+++ server/docker-run.sh
@@ -1,19 +1,18 @@
-#Delete
+IMAGE="h5r-server"
+CONTAINER="$IMAGE-container"
 
-docker rm h5r-server-container
-docker rmi h5r-server
+
+#Delete
+docker rm $CONTAINER
+docker rmi $IMAGE
 
 # Build image
-docker build -t h5r-server .
+docker build -t $IMAGE .
 
 # --build-arg SPRING_CONFIG_LOCATION=*** \
 
-# H5R_PATH=$(pwd)/node_modules/h5recorder
-H5R_PATH=$(pwd)/h5recorder
-
 # Run
-# -v $H5R_PATH:/app/node_modules/h5recorder \
 docker run \
 -p 5000:5000 \
---name h5r-server-container \
-h5r-server
+--name $CONTAINER \
+$IMAGE
