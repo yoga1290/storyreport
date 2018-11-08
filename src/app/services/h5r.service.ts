d3809c1d77db446777176b0c6fda22a1a30b3963
Youssef Gamil <yoga1290@gmail.com>
2018-11-08 19:59:04 +0200

    WIP progress bar

--- src/app/services/h5r.service.ts
+++ src/app/services/h5r.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+// import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 
+// https://stackblitz.com/angular/rlqbgldgerxb?file=src%2Fapp%2Fuploader%2Fuploader.service.ts
+import {
+  HttpClient, HttpEvent, HttpEventType, HttpProgressEvent,
+  HttpRequest, HttpResponse, HttpErrorResponse
+} from '@angular/common/http';
 
+// import { of } from 'rxjs';
+import { catchError, last, map, tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -63,7 +70,20 @@ export class H5RService {
     });
 
     console.log('formData', formData);
-    // return this.http.post<any>("https://storyreport.herokuapp.com/submit", formData, httpOptions);//.pipe( //login
-    return this.http.post<any>(`${environment.apiUrl}/drive/upload?access_token=${accessToken}&refresh_token=${refreshToken}`, formData, httpOptions);
+
+    const req = new HttpRequest('POST',
+      `${environment.apiUrl}/drive/upload?access_token=${accessToken}&refresh_token=${refreshToken}`,
+      formData,
+      httpOptions);
+    return this.http.request(req).pipe(
+      map(console.log),
+      tap(console.log),
+      last(), // return last (completed) message to caller
+      catchError((err)=>{
+          console.log('error', err)
+          return 'error'
+      })
+    );
+    // return this.http.post<any>(`${environment.apiUrl}/drive/upload?access_token=${accessToken}&refresh_token=${refreshToken}`, formData, httpOptions);
   }
 }
